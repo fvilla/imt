3ddfbf645a1c59a1dd2f2b332a165bcbf2dc047c
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-06-11 22:18:27 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/osgi@3073 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ org.integratedmodelling.knowledge.foundational/src/org/integratedmodelling/knowledge/foundational/Activator.java
@@ -0,0 +1,20 @@
+package org.integratedmodelling.knowledge.foundational;
+
+import org.integratedmodelling.thinklab.ThinklabActivator;
+
+public class Activator extends ThinklabActivator {
+
+	@Override
+	protected void doStart() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void doStop() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
