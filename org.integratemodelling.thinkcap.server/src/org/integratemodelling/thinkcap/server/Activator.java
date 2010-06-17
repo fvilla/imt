fe6489cac017891aff8b1e995b2e086d6acbda94
fvilla <fvilla@ca20354d-2f28-0410-8711-9259e250f5a2>
2010-06-17 21:09:10 +0000

    git-svn-id: https://imt.svn.sourceforge.net/svnroot/imt/trunk/osgi@3139 ca20354d-2f28-0410-8711-9259e250f5a2

---
+++ org.integratemodelling.thinkcap.server/src/org/integratemodelling/thinkcap/server/Activator.java
@@ -0,0 +1,35 @@
+package org.integratemodelling.thinkcap.server;
+
+import org.integratedmodelling.thinklab.ThinklabActivator;
+
+public class Activator extends ThinklabActivator {
+
+	@Override
+	protected void doStart() throws Exception {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * TODO ensure that thinkcap.core is started
+		 */
+		
+		/*
+		 * TODO
+		 * lookup all thinkcap application bundles, start them and export their apps 
+		 */
+		
+		/*
+		 * TODO publish main thinkcap application and manager
+		 */
+		
+		/*
+		 * start server
+		 */
+	}
+
+	@Override
+	protected void doStop() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
